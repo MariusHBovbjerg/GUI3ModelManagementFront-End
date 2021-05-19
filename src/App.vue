4cdb9542e48f4818264acae1e0daa724b5f003d8
KunJakob <jakobhkristensen@gmail.com>
2021-05-19 21:40:29 +0200

    konverteret til typescript

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-    <div id="app">
-        <Login/>
-        <Manager/>
-        <Model/>
-        <Job/>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
 </template>
 
-<script>
-    import store from './Store/Store.js';
-    import Login from './components/LoginComponent.vue';
-    import Manager from './components/ManagerComponent.vue';
-    import Model from './components/ModelComponent.vue';
-    import Job from './components/JobComponent.vue';
-    export default {
-        name: 'app',
-        store,
-        components: {
-            Login,
-            Manager,
-            Model,
-            Job
-        }
-    };
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
-
---
+++ src/Models/Expense.ts
@@ -0,0 +1,7 @@
+export interface Expense {
+  modelId: number;
+  jobId: number;
+  date: Date;
+  text: string;
+  amount: number;
+}
